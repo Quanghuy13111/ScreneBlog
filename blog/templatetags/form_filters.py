661558d8c050c6174858b9e0440ca82fa9b247e2
Quanghuy13111 <quanghuyvu131211@gmail.com>
2025-12-10 21:56:03 +0700

    update code

--- blog/templatetags/form_filters.py
+++ blog/templatetags/form_filters.py
@@ -1,11 +1,19 @@
 from django import template
+from django.forms.boundfield import BoundField
+import re
 
 register = template.Library()
 
 @register.filter(name='attr')
-def attr(field, css):
-    attrs = {}
-    for pair in css.split('|'):
-        key, value = pair.split(':', 1)
-        attrs[key.strip()] = value.strip()
-    return field.as_widget(attrs=attrs)
+def attr(field, attribute_string):
+    if isinstance(field, BoundField):
+        attrs = {}
+        # A robust regex to parse attributes. It looks for a key followed by a colon,
+        # then captures everything until it hits the next key (or the end of the string).
+        # This correctly handles values with colons, like in the 'style' attribute.
+        for key, value in re.findall(r'(\w+):((?:(?! \w+:).)*)', attribute_string):
+            attrs[key] = value.strip()
+        return field.as_widget(attrs=attrs)
+    else:
+        # If it's not a BoundField, return the original field value to prevent AttributeError
+        return field
